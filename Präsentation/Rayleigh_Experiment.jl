63828aa3ca12d07dfa4d74126f2b6ff3a8f4f44d
TomRiem <riemertom720@googlemail.com>
2021-06-10 14:21:43 +0200

    updates presentation.

--- Präsentation/Rayleigh_Experiment.jl
+++ Präsentation/Rayleigh_Experiment.jl
@@ -2,23 +2,12 @@ using Manopt, Manifolds, Random
 Random.seed!(42)
 n = 50
 A = randn(n,n)
-A = ( A + A' ) / 2
-F(::Sphere, p::Array{Float64,1}) = p' * A * p
-gradF(::Sphere, p::Array{Float64,1}) = 2 * (A * p - p * p' * A * p)
-HessF(::Sphere, p::Array{Float64,1}, X::Array{Float64,1}) = 2 * (A * X - p * p' * A * X - X * p' * A * p - p * p' * X * p' * A * p)
-M = Sphere(n - 1)
+A = (A+A')/2
+F(::Sphere, p::Array{Float64,1}) = p'*A*p
+gradF(::Sphere, p::Array{Float64,1}) = 2*(A*p-p*p'*A*p)
+M = Sphere(n-1)
 x = random_point(M)
-trust_regions!(
-    M,
-    F,
-    gradF,
-    ApproxHessianSymmetricRankOne(M, x, gradF; nu = sqrt(eps(Float64))),
-    x;
-    stopping_criterion=StopWhenAny(
-        StopAfterIteration(500), StopWhenGradientNormLess(10^(-6))
-    ),
-    retraction_method=ProjectionRetraction(),
-    θ=0.1,
-    κ=0.9,
-    trust_region_radius=1.0
-)
+
+trust_regions!(M, F, gradF,
+ApproxHessianSymmetricRankOne(M, x, gradF; nu=1e-8),
+x; retraction_method=ProjectionRetraction())
